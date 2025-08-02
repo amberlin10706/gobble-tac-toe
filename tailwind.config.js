cac2d455ca4ccc41b4767b87abd2859fe4e48004
amberlin10706 <yyyy2248@hotmail.com>
2025-08-02 09:57:56 +0800

    add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", // 確保有包含你寫 component 的目錄
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
